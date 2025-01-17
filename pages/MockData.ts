1261d6a6d1e2df941b6ed8ba47bc4149975dc4a1
crabIsNotAvailable <martinyunilsen3@hotmail.com>
2025-01-17 09:46:01 +0100

    initial commit

---
+++ pages/MockData.ts
@@ -0,0 +1,19 @@
+export default interface ClientDetailProps {
+    KontoID: number;
+    Kundegruppe: string;
+    Valuta: string;
+    Produkttype: string;
+    Rentebane: string;
+    Forfallsdato: number;
+    Terminer: number;
+    Lånetype: string;
+    Hovedstol: number;
+    Rente: number;
+    Gebyr: number;
+    Forfalterenter: number;
+    Avdragbetalt: number;
+    Termingebyr: number;
+    Utestående: number;
+    Etablering: number;
+}
+
---
+++ pages/index.js
@@ -0,0 +1,141 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid2 from '@mui/material/Grid2';
+
+const Item= styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(4),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      <Box sx={{ width: '50%' }}>
+        <Grid2 container  spacing={1}>
+          <Grid2 container size={2}>
+            <Item>1</Item>
+            <Item>2</Item>
+          </Grid2>
+          <Grid2 container size={2}>
+            <Item>3</Item>
+            <Item>4</Item>
+          </Grid2>
+          <Grid2 container size={2}>
+            <Item>5</Item>
+            <Item>6</Item>
+          </Grid2>
+          <Grid2 container size={2}>
+            <Item>7</Item>
+            <Item>8</Item>
+          </Grid2>
+          <Grid2 container size={2}>
+            <Item>9</Item>
+            <Item>10</Item>
+          </Grid2>
+          <Grid2 container size={2}>
+            <Item>11</Item>
+            <Item>12</Item>
+          </Grid2>
+        </Grid2>
+      </Box>
+
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
